dad904909aa361cf7c547526191793390a45a413
FrankTII <frank_training@outlook.com>
2023-06-14 19:25:27 -0600

    Upate S7 files  from s7Gradle310

--- src/main/java/org/bedu/equipo6/controllers/mappers/ClienteMapper.java
+++ src/main/java/org/bedu/equipo6/controllers/mappers/ClienteMapper.java
@@ -1,7 +1,11 @@
 package org.bedu.equipo6.controllers.mappers;
 
+import org.bedu.equipo6.persistence.entities.Cliente;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface ClienteMapper {
+    Cliente clienteModelToClienteEntity(org.bedu.equipo6.model.Cliente clienteModel);
+
+    org.bedu.equipo6.model.Cliente clienteEntityToClienteModel(Cliente cliente);
 }
